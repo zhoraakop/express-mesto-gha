b603e2df97cbe0b6b255644145299e9e7fbf4e9b
Georgy Akopov <akopovzh@yandex.ru>
2023-10-16 19:55:52 +0300

    fixed/1/review

--- controllers/users.js
+++ controllers/users.js
@@ -17,7 +17,7 @@ const createUser = (req, res) => {
     })
     .catch((err) => {
       if (err.name === 'ValidationError') {
-        return res.status(HTTP_STATUS_BAD_REQUEST).send({ message: err.message });
+        return res.status(HTTP_STATUS_BAD_REQUEST).send({ message: 'Переданы некорреткные данные' });
       }
       return res.status(HTTP_STATUS_INTERNAL_SERVER_ERROR).send({ message: 'На сервере произошла ошибка' });
     });
@@ -26,7 +26,10 @@ const createUser = (req, res) => {
 const getUserById = (req, res) => {
   const id = req.params.userId;
   userModel.findById(id).then((user) => {
-    res.status(HTTP_STATUS_OK).send(user);
+    if (user) {
+      return res.status(HTTP_STATUS_OK).send(user);
+    }
+    return res.status(HTTP_STATUS_OK).send({});
   }).catch((err) => {
     if (err.name === 'CastError') {
       return res.status(HTTP_STATUS_BAD_REQUEST).send({ message: 'Пользователь с таким id не найден' });
