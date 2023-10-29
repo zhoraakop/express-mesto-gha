3fc2a1dcfa9e7c5cbdbb49fe8c58c54aca6c00e3
Georgy Akopov <akopovzh@yandex.ru>
2023-10-29 16:55:47 +0300

    fixxed/autoTest

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -4,12 +4,11 @@ const UnauthorizedError = require('../errors/Unauthorized-error');
 
 const auth = (req, res, next) => {
   let payload;
-  const { authorization } = req.headers;
+  const token = req.headers.authorization;
 
-  if (!authorization || !authorization.startsWith('Bearer ')) {
+  if (!token) {
     return next(new UnauthorizedError('Необходима авторизация'));
   }
-  const token = authorization.replace('Bearer ', '');
   try {
     payload = jwt.verify(token, 'SECRET_KEY');
   } catch (err) {
